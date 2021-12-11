c83ae6bd65d4de24215fdacc09d44a2cde71eaa6
claesdev <claeswdev@gmail.com>
2021-12-11 10:54:03 +0100

    Add message components

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const subjects = []
-const presents = []
-const objects = []
+const subjects = ['You', 'Your brother', 'Your sister', 'Your girlfriend', 'Your mother', 'Your father', 'The cat', 'Santa Claus']
+const presents = ['a bicycle', 'a train set', 'a comfy sweater', 'a pair of slippers', 'a magazine subscription', 'a pretty necklace']
+const objects = ['me', 'you', 'the neighbour', 'the janitor', 'the janitors dog', 'my mom', 'my dad', 'some homeless guy', 'the cat']
